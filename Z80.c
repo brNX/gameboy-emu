42b6047fb2ff38025ca8410c7387b84260857b9e
Bruno Gouveia <bgouveia@gmail.com>
2010-07-01 04:09:47 +0100

    bit commands done

--- Z80.c
+++ Z80.c
@@ -832,7 +832,7 @@ int execute(int ncycles)
 			LD_HLSP(readMem(pc++));
 			break;
 
-		/*****************GMB Rotate- and Shift-Commands*********************/
+		/*****************GMB Rotate- , Shift- and Singlebit Operation Commands*********************/
 
 		case 0x07: //RLCA
 			F = (A & 0x80) >> 3;
@@ -1145,9 +1145,646 @@ int execute(int ncycles)
 				case 0x37: //SWAP A
 					SWAP (A);
 					break;
+
+				/***bit opcodes**/
+
+				/*BIT b,r*/
+				case 0x40: //BIT 0,B
+  					BIT(0,B);
+					break;
+				case 0x41: //BIT 0,C
+  					BIT(0,C);
+					break;
+				case 0x42: // BIT 0,D
+  					BIT(0,D);
+					break;
+				case 0x43: // BIT 0,E
+  					BIT(0,E);
+					break;
+				case 0x44: // BIT 0,H
+  					BIT(0,H);
+					break;
+				case 0x45: // BIT 0,L
+  					BIT(0,L);
+					break;
+				case 0x47: // BIT 0,A
+  					BIT(0,A);
+					break;
+				case 0x48: // BIT 1,B
+  					BIT(1,B);
+					break;
+				case 0x49: // BIT 1,C
+  					BIT(1,C);
+					break;
+				case 0x4A: // BIT 1,D
+  					BIT(1,D);
+					break;
+				case 0x4B: // BIT 1,E
+  					BIT(1,E);
+					break;
+				case 0x4C: // BIT 1,H
+  					BIT(1,H);
+					break;
+				case 0x4D: // BIT 1,L
+  					BIT(1,L);
+					break;
+				case 0x4F: // BIT 1,A
+  					BIT(1,A);
+					break;
+				case 0x50: // BIT 2,B
+  					BIT(2,B);
+					break;
+				case 0x51: // BIT 2,C
+  					BIT(2,C);
+					break;
+				case 0x52: // BIT 2,D
+  					BIT(2,D);
+					break;
+				case 0x53: // BIT 2,E
+  					BIT(2,E);
+					break;
+				case 0x54: // BIT 2,H
+  					BIT(2,H);
+					break;
+				case 0x55: // BIT 2,L
+  					BIT(2,L);
+					break;
+				case 0x57: // BIT 2,A
+  					BIT(2,A);
+					break;
+				case 0x58: // BIT 3,B
+  					BIT(3,B);
+					break;
+				case 0x59: // BIT 3,C
+  					BIT(3,C);
+					break;
+				case 0x5A: // BIT 3,D
+  					BIT(3,D);
+					break;
+				case 0x5B: // BIT 3,E
+  					BIT(3,E);
+					break;
+				case 0x5C: // BIT 3,H
+  					BIT(3,H);
+					break;
+				case 0x5D: // BIT 3,L
+  					BIT(3,L);
+					break;
+				case 0x5F: // BIT 3,A
+  					BIT(3,A);
+					break;
+				case 0x60: // BIT 4,B
+  					BIT(4,B);
+					break;
+				case 0x61: // BIT 4,C
+  					BIT(4,C);
+					break;
+				case 0x62: // BIT 4,D
+  					BIT(4,D);
+					break;
+				case 0x63: // BIT 4,E
+  					BIT(4,E);
+					break;
+				case 0x64: // BIT 4,H
+  					BIT(4,H);
+					break;
+				case 0x65: // BIT 4,L
+  					BIT(4,L);
+					break;
+				case 0x67: // BIT 4,A
+  					BIT(4,A);
+					break;
+				case 0x68: // BIT 5,B
+  					BIT(5,B);
+					break;
+				case 0x69: // BIT 5,C
+  					BIT(5,C);
+					break;
+				case 0x6A: // BIT 5,D
+  					BIT(5,D);
+					break;
+				case 0x6B: // BIT 5,E
+  					BIT(5,E);
+					break;
+				case 0x6C: // BIT 5,H
+  					BIT(5,H);
+					break;
+				case 0x6D: // BIT 5,L
+  					BIT(5,L);
+					break;
+				case 0x6F: // BIT 5,A
+  					BIT(5,A);
+					break;
+				case 0x70: // BIT 6,B
+  					BIT(6,B);
+					break;
+				case 0x71: // BIT 6,C
+  					BIT(6,C);
+					break;
+				case 0x72: // BIT 6,D
+  					BIT(6,D);
+					break;
+				case 0x73: // BIT 6,E
+  					BIT(6,E);
+					break;
+				case 0x74: // BIT 6,H
+  					BIT(6,H);
+					break;
+				case 0x75: // BIT 6,L
+  					BIT(6,L);
+					break;
+				case 0x77: // BIT 6,A
+  					BIT(6,A);
+					break;
+				case 0x78: // BIT 7,B
+  					BIT(7,B);
+					break;
+				case 0x79: // BIT 7,C
+  					BIT(7,C);
+					break;
+				case 0x7A: // BIT 7,D
+  					BIT(7,D);
+					break;
+				case 0x7B: // BIT 7,E
+  					BIT(7,E);
+					break;
+				case 0x7C: // BIT 7,H
+  					BIT(7,H);
+					break;
+				case 0x7D: // BIT 7,L
+  					BIT(7,L);
+					break;
+				case 0x7F: // BIT 7,A
+					BIT(7,A);
+					break;
+
+				case 0x46: // BIT 0,(HL)
+  					tempbyte=readMem(HL);
+					BIT(0,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x4E: // BIT 1,(HL)
+  					tempbyte=readMem(HL);
+					BIT(1,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x56: // BIT 2,(HL)
+  					tempbyte=readMem(HL);
+					BIT(2,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x5E: // BIT 3,(HL)
+  					tempbyte=readMem(HL);
+					BIT(3,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x66: // BIT 4,(HL)
+  					tempbyte=readMem(HL);
+					BIT(4,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x6E: // BIT 5,(HL)
+  					tempbyte=readMem(HL);
+					BIT(5,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x76: // BIT 6,(HL)
+  					tempbyte=readMem(HL);
+					BIT(6,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x7E: // BIT 7,(HL)
+  					tempbyte=readMem(HL);
+					BIT(7,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+
+				/*SET b,r */
+				case  0xC0:// SET 0,B
+  					SET(0,B);
+					break;
+				case  0xC1:// SET 0,C
+  					SET(0,C);
+					break;
+				case  0xC2:// SET 0,D
+  					SET(0,D);
+					break;
+				case  0xC3:// SET 0,E
+  					SET(0,E);
+					break;
+				case  0xC4:// SET 0,H
+  					SET(0,H);
+					break;
+				case  0xC5:// SET 0,L
+  					SET(0,L);
+					break;
+				case  0xC6:// SET 0,(HL)
+  					tempbyte=readMem(HL);
+					SET(0,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xC7:// SET 0,A
+  					SET(0,A);
+					break;
+				case  0xC8:// SET 1,B
+  					SET(1,B);
+					break;
+				case  0xC9:// SET 1,C
+  					SET(1,C);
+					break;
+				case  0xCA:// SET 1,D
+  					SET(1,D);
+					break;
+				case  0xCB:// SET 1,E
+  					SET(1,E);
+					break;
+				case  0xCC:// SET 1,H
+  					SET(1,H);
+					break;
+				case  0xCD:// SET 1,L
+  					SET(1,L);
+					break;
+				case  0xCE:// SET 1,(HL)
+  					tempbyte=readMem(HL);
+					SET(1,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xCF:// SET 1,A
+  					SET(1,A);
+					break;
+				case  0xD0:// SET 2,B
+  					SET(2,B);
+					break;
+				case  0xD1:// SET 2,C
+  					SET(2,C);
+					break;
+				case  0xD2:// SET 2,D
+  					SET(2,D);
+					break;
+				case  0xD3:// SET 2,E
+  					SET(2,E);
+					break;
+				case  0xD4:// SET 2,H
+  					SET(2,H);
+					break;
+				case  0xD5:// SET 2,L
+  					SET(2,L);
+					break;
+				case  0xD6:// SET 2,(HL)
+  					tempbyte=readMem(HL);
+					SET(2,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xD7:// SET 2,A
+  					SET(2,A);
+					break;
+				case  0xD8:// SET 3,B
+  					SET(3,B);
+					break;
+				case  0xD9:// SET 3,C
+  					SET(3,C);
+					break;
+				case  0xDA:// SET 3,D
+  					SET(3,D);
+					break;
+				case  0xDB:// SET 3,E
+  					SET(3,E);
+					break;
+				case  0xDC:// SET 3,H
+  					SET(3,H);
+					break;
+				case  0xDD:// SET 3,L
+  					SET(3,L);
+					break;
+				case  0xDE:// SET 3,(HL)
+  					tempbyte=readMem(HL);
+					SET(3,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xDF:// SET 3,A
+  					SET(3,A);
+					break;
+				case  0xE0:// SET 4,B
+  					SET(4,B);
+					break;
+				case  0xE1:// SET 4,C
+  					SET(4,C);
+					break;
+				case  0xE2:// SET 4,D
+  					SET(4,D);
+					break;
+				case  0xE3:// SET 4,E
+  					SET(4,E);
+					break;
+				case  0xE4:// SET 4,H
+  					SET(4,H);
+					break;
+				case  0xE5:// SET 4,L
+  					SET(4,L);
+					break;
+				case  0xE6:// SET 4,(HL)
+  					tempbyte=readMem(HL);
+					SET(4,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xE7:// SET 4,A
+  					SET(4,A);
+					break;
+				case  0xE8:// SET 5,B
+  					SET(5,B);
+					break;
+				case  0xE9:// SET 5,C
+  					SET(5,C);
+					break;
+				case  0xEA:// SET 5,D
+  					SET(5,D);
+					break;
+				case  0xEB:// SET 5,E
+  					SET(5,E);
+					break;
+				case  0xEC:// SET 5,H
+  					SET(5,H);
+					break;
+				case  0xED:// SET 5,L
+  					SET(5,L);
+					break;
+				case  0xEE:// SET 5,(HL)
+  					tempbyte=readMem(HL);
+					SET(5,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xEF:// SET 5,A
+  					SET(5,A);
+					break;
+				case  0xF0:// SET 6,B
+  					SET(6,B);
+					break;
+				case  0xF1:// SET 6,C
+  					SET(6,C);
+					break;
+				case  0xF2:// SET 6,D
+  					SET(6,D);
+					break;
+				case  0xF3:// SET 6,E
+  					SET(6,E);
+					break;
+				case  0xF4:// SET 6,H
+  					SET(6,H);
+					break;
+				case  0xF5:// SET 6,L
+  					SET(6,L);
+					break;
+				case  0xF6:// SET 6,(HL)
+  					tempbyte=readMem(HL);
+					SET(6,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xF7:// SET 6,A
+  					SET(6,A);
+					break;
+				case  0xF8:// SET 7,B
+  					SET(7,B);
+					break;
+				case  0xF9:// SET 7,C
+  					SET(7,C);
+					break;
+				case  0xFA:// SET 7,D
+  					SET(7,D);
+					break;
+				case  0xFB:// SET 7,E
+  					SET(7,E);
+					break;
+				case  0xFC:// SET 7,H
+  					SET(7,H);
+					break;
+				case  0xFD:// SET 7,L
+  					SET(7,L);
+					break;
+				case  0xFE:// SET 7,(HL)
+  					tempbyte=readMem(HL);
+					SET(7,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case  0xFF:// SET 7,A
+  					SET(7,A);
+					break;
+
+				/*RES b,r*/
+				case 0x80:// RES 0,B
+  					RES(0,B);
+					break;
+				case 0x81:// RES 0,C
+  					RES(0,C);
+					break;
+				case 0x82:// RES 0,D
+  					RES(0,D);
+					break;
+				case 0x83:// RES 0,E
+  					RES(0,E);
+					break;
+				case 0x84:// RES 0,H
+  					RES(0,H);
+					break;
+				case 0x85:// RES 0,L
+  					RES(0,L);
+					break;
+				case 0x86:// RES 0,(HL)
+  					tempbyte=readMem(HL);
+					RES(0,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x87:// RES 0,A
+  					RES(0,A);
+					break;
+				case 0x88:// RES 1,B
+  					RES(1,B);
+					break;
+				case 0x89:// RES 1,C
+  					RES(1,C);
+					break;
+				case 0x8A:// RES 1,D
+  					RES(1,D);
+					break;
+				case 0x8B:// RES 1,E
+  					RES(1,E);
+					break;
+				case 0x8C:// RES 1,H
+  					RES(1,H);
+					break;
+				case 0x8D:// RES 1,L
+  					RES(1,L);
+					break;
+				case 0x8E:// RES 1,(HL)
+  					tempbyte=readMem(HL);
+					RES(1,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x8F:// RES 1,A
+  					RES(1,A);
+					break;
+				case 0x90:// RES 2,B
+  					RES(2,B);
+					break;
+				case 0x91:// RES 2,C
+  					RES(2,C);
+					break;
+				case 0x92:// RES 2,D
+  					RES(2,D);
+					break;
+				case 0x93:// RES 2,E
+  					RES(2,E);
+					break;
+				case 0x94:// RES 2,H
+  					RES(2,H);
+					break;
+				case 0x95:// RES 2,L
+  					RES(2,L);
+					break;
+				case 0x96:// RES 2,(HL)
+  					tempbyte=readMem(HL);
+					RES(2,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x97:// RES 2,A
+  					RES(2,A);
+					break;
+				case 0x98:// RES 3,B
+  					RES(3,B);
+					break;
+				case 0x99:// RES 3,C
+  					RES(3,C);
+					break;
+				case 0x9A:// RES 3,D
+  					RES(3,D);
+					break;
+				case 0x9B:// RES 3,E
+  					RES(3,E);
+					break;
+				case 0x9C:// RES 3,H
+  					RES(3,H);
+					break;
+				case 0x9D:// RES 3,L
+  					RES(3,L);
+					break;
+				case 0x9E:// RES 3,(HL)
+  					tempbyte=readMem(HL);
+					RES(3,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0x9F:// RES 3,A
+  					RES(3,A);
+					break;
+				case 0xA0:// RES 4,B
+  					RES(4,B);
+					break;
+				case 0xA1:// RES 4,C
+  					RES(4,C);
+					break;
+				case 0xA2:// RES 4,D
+  					RES(4,D);
+					break;
+				case 0xA3:// RES 4,E
+  					RES(4,E);
+					break;
+				case 0xA4:// RES 4,H
+  					RES(4,H);
+					break;
+				case 0xA5:// RES 4,L
+  					RES(4,L);
+					break;
+				case 0xA6:// RES 4,(HL)
+  					tempbyte=readMem(HL);
+					RES(4,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0xA7:// RES 4,A
+  					RES(4,A);
+					break;
+				case 0xA8:// RES 5,B
+  					RES(5,B);
+					break;
+				case 0xA9:// RES 5,C
+  					RES(5,C);
+					break;
+				case 0xAA:// RES 5,D
+  					RES(5,D);
+					break;
+				case 0xAB:// RES 5,E
+  					RES(5,E);
+					break;
+				case 0xAC:// RES 5,H
+  					RES(5,H);
+					break;
+				case 0xAD:// RES 5,L
+  					RES(5,L);
+					break;
+				case 0xAE:// RES 5,(HL)
+  					tempbyte=readMem(HL);
+					RES(5,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0xAF:// RES 5,A
+  					RES(5,A);
+					break;
+				case 0xB0:// RES 6,B
+  					RES(6,B);
+					break;
+				case 0xB1:// RES 6,C
+  					RES(6,C);
+					break;
+				case 0xB2:// RES 6,D
+  					RES(6,D);
+					break;
+				case 0xB3:// RES 6,E
+  					RES(6,E);
+					break;
+				case 0xB4:// RES 6,H
+  					RES(6,H);
+					break;
+				case 0xB5:// RES 6,L
+  					RES(6,L);
+					break;
+				case 0xB6:// RES 6,(HL)
+  					tempbyte=readMem(HL);
+					RES(6,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0xB7:// RES 6,A
+  					RES(6,A);
+					break;
+				case 0xB8:// RES 7,B
+  					RES(7,B);
+					break;
+				case 0xB9:// RES 7,C
+  					RES(7,C);
+					break;
+				case 0xBA:// RES 7,D
+  					RES(7,D);
+					break;
+				case 0xBB:// RES 7,E
+  					RES(7,E);
+					break;
+				case 0xBC:// RES 7,H
+  					RES(7,H);
+					break;
+				case 0xBD:// RES 7,L
+  					RES(7,L);
+					break;
+				case 0xBE:// RES 7,(HL)
+  					tempbyte=readMem(HL);
+					RES(7,tempbyte);
+					writeMem(HL,tempbyte);
+					break;
+				case 0xBF:// RES 7,A
+  					RES(7,A);
+					break;
 				default:
 					break;
 			}
+
+
+		/*****************GMB Jumpcommands*********************/
+
 
 
 
--- Z80.h
+++ Z80.h
@@ -196,6 +196,23 @@
 	val = (val >>4) | (val << 4);\
 	F = (F & ~Z_FLAG) | (-(val==0) & Z_FLAG)
 
+//TODO : verificar o BIT
+#define BIT(n,val)\
+	if(((0x1 << n) & val)==0)\
+		F |= Z_FLAG;\
+	else \
+		F &= ~Z_FLAG; \
+	F &= ~N_FLAG;\
+	F |= H_FLAG
+
+//TODO : verificar o SET
+#define SET(n,val)\
+	val |= (0x1 << n)
+
+//TODO : verificar o RES
+#define RES(n,val)\
+	val &= ~(0x1 << n)
+
 
 
 #include <stdint.h>
