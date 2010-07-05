f374de1275dcb61342335d88e940c673ef0cb37a
Bruno Gouveia <bgouveia@gmail.com>
2010-07-05 21:09:22 +0100

    extern

--- memory.c
+++ memory.c
@@ -7,12 +7,12 @@
 #include "memory.h"
 
 //TODO: for now
-inline uint8_t readMem(uint16_t address)
+extern inline uint8_t readMem(uint16_t address)
 {
 	return (gb_memory[address]);
 }
 
-inline void writeMem(uint16_t address, uint8_t value)
+extern inline void writeMem(uint16_t address, uint8_t value)
 {
 	gb_memory[address] = value;
 }
